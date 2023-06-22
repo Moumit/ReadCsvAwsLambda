fea54324c2720e8e4a50f2ca675f55778e398020
Moumit Mondal <moumit.mondal@gmail.com>
2023-06-22 21:46:38 +0530

    Completed

--- ReadCsvAwsLambda/src/ReadCsvAwsLambda/Function.cs
+++ ReadCsvAwsLambda/src/ReadCsvAwsLambda/Function.cs
@@ -1,8 +1,15 @@
+using Amazon;
+using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.DocumentModel;
+using Amazon.DynamoDBv2.Model;
 using Amazon.Lambda.Core;
 using Amazon.Lambda.S3Events;
+using Amazon.Runtime.Internal.Transform;
 using Amazon.S3;
 using Amazon.S3.Model;
 using System.IO;
+using System.Linq;
+using System.Text.Json;
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
@@ -21,6 +28,16 @@ namespace ReadCsvAwsLambda
         public async Task FunctionHandlerAsync(S3Event s3Event)
         {
             AmazonS3Client s3Client= new AmazonS3Client();
+
+
+            var lstHeaders = "HertzUnitArea,HertzUnitID,VIN,Mileage".Split(',');
+
+
+            AmazonDynamoDBConfig clientConfig = new AmazonDynamoDBConfig();
+            // This client will access the US East 1 region.
+            clientConfig.RegionEndpoint = RegionEndpoint.USEast1;
+            AmazonDynamoDBClient dBClient = new AmazonDynamoDBClient(clientConfig);
+
             foreach (var record in s3Event.Records)
             {
                 var tempFile = record.S3;
@@ -32,19 +49,42 @@ namespace ReadCsvAwsLambda
                     
                 };
 
-                Console.WriteLine("Begin reading");
+                Console.WriteLine("Begin reading"); 
 
                 using (var res = await s3Client.GetObjectAsync(request))
                 {
-                    int count = 0;
+                    //int count = 0;
                     StreamReader sReader = new StreamReader(res.ResponseStream); //Time out here
 
                     while (!sReader.EndOfStream) {
                         string? line = sReader.ReadLine();
-                        Console.WriteLine(count+" : "+ line);
-                        count++;
+                        //Console.WriteLine(count+" : "+ line);
+
+                        var values = line.Split(',').ToList();
+                        var finalRow= new Dictionary<string, AttributeValue>();                      
+
+                        finalRow.Add("NoPrimary", new AttributeValue { S= Guid.NewGuid().ToString() });
+                        int countIndex = 0;
+                        foreach (var value in lstHeaders)
+                        {
+                            var valueItem = values.ElementAtOrDefault(countIndex++) ?? string.Empty;
+                            finalRow.Add(value, new AttributeValue { S= valueItem });
+                        }
+                        var createItemRequest = new PutItemRequest
+                        {
+                            TableName = "DDUS",
+                            Item = finalRow
+                        };
+                        
+                        //Console.WriteLine(JsonSerializer.Serialize(finalRow));
+
+                        var response = await dBClient.PutItemAsync(createItemRequest);
+
+                        
                     }                    
                 }
+
+
 
                 Console.WriteLine("End reading");
 
